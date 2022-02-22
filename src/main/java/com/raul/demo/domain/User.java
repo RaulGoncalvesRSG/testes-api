577c5f66d1449d9dc5ac56bdc494583ae13eb2d8
Raul Gonçalves <raulsantos121@gmail.com>
2022-02-22 18:54:23 -0300

    Criação do projeto

---
+++ src/main/java/com/raul/demo/domain/User.java
@@ -0,0 +1,28 @@
+package com.raul.demo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Data inclui o @Getter @Setter @ToString e @EqualsAndHashCode. Recomendado apenas qnd tiver poucos atributos
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+}
